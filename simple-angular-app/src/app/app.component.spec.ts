7f8bc2876da0ce6b9fa509e76b421ae5f6d4cb98
tadeusz-cirocki <tadeusz.cirocki@widelab.co>
2022-10-24 16:47:35 +0200

    title update

--- simple-angular-app/src/app/app.component.spec.ts
+++ simple-angular-app/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('simple-angular-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('updated! simple-angular-app app is running!');
   });
 });
